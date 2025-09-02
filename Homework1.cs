9cd84b72e1771318261570d0512a4fbb5de7c14e
RSierra18 <resierra1@buffs.wtamu.edu>
2025-09-01 20:41:19 -0500

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework1;
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //given values
+        double x = 2.5;
+        double y = 3.3;
+        double z = ((4 * x * x))+ (3 * y);
+
+    
+
+        Console.WriteLine("the value of z is:" + z );
+    }
+}
